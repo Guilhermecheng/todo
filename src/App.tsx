618edf58a930de0b91b63daf80ec0b7658889dae
Guilhermecheng <guilherme.cheng@gmail.com>
2023-02-23 12:24:47 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center relative">
+      <div className="w-full h-[200px] bg-[#0D0D0D] flex items-center justify-center">
+        <h1 className="text-white">
+          to<span>do</span>
+        </h1>
+      </div>
+
+      <section className="absolute top-[172px] flex gap-x-4 container">
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          className="w-full rounded-md bg-[#262626] px-4 py-2"
+        />
+        <button className="bg-[#1E6F9F] px-2 py-4 rounded-lg w-24 font-semibold text-white">
+          Criar +
+        </button>
+      </section>
+
+      <section className="container mt-16">
+        <div className="w-full flex relative pb-4 border-b-2 border-[#333] font-semibold text-[#8284FA]">
+          <h2 className="justify-start">Tarefas criadas</h2>
+
+          <h2 className="absolute right-0">Concluídas</h2>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
